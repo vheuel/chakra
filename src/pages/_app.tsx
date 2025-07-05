0667daed1393f278436cd8187681eab97f932eba
vheuel <vheuels@gmail.com>
2025-07-05 22:30:45 +0700

    set privy app id untuk auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import "@/styles/globals.css";
+import { PrivyProvider } from "@privy-io/react-auth";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+
+const PRIVY_APP_ID = "cmcqee3i700adic0nsfam1rdb";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PrivyProvider
+      appId={PRIVY_APP_ID}
+      config={{
+        appearance: {
+          theme: "dark",
+        },
+        embeddedWallets: {
+          createOnLogin: "users-without-wallets",
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </PrivyProvider>
+  );
 }
