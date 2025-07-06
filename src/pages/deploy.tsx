c37f328ad9d63ab2ec206301daf8378e49c8f52b
vheuel <vheuels@gmail.com>
2025-07-06 12:40:58 +0700

    Fix ESLint errors in deploy.tsx

--- src/pages/deploy.tsx
+++ src/pages/deploy.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useWallets } from "@privy-io/react-auth";
 import { useState } from "react";
 import { createPublicClient, createWalletClient, http } from "viem";
